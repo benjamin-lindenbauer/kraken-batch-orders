eae12334ffd260daa3fa44593f73869e663103bd
benjamin-lindenbauer <benjamin.lindenbauer@gmail.com>
2024-12-10 16:04:32 +0100

    Added manifest and service-worker

---
+++ public/service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'kraken-batch-orders-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/utils.js',
+  '/favicon.svg',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
